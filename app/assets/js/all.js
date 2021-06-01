7c4090802696fcc66c4ee71dccafe316033666ad
y75434 <she851213@gmail.com>
2021-06-01 22:34:41 +0800

    新增頁面

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,35 @@
-console.log('Hello!');
-
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+var swiper = new Swiper('.topChoices .swiper-container', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints:{
+    0:{
+      slidesPerView: 1,
+    },
+    768:{
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  }
 });
+
+
+
+
+
+
+//AOS
+AOS.init();
